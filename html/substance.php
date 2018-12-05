50382f73cc352402d93430a41d35ccb3a8c6e389
Hubert <hdudowicz@gmail.com>
2018-12-05 02:53:30 +0000

    A commit

--- html/substance.php
+++ html/substance.php
@@ -17,6 +17,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="scripts/livesearch.js"></script>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">SubstanceWiki</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,8 +37,9 @@
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search">
+            <input class="form-control mr-sm-2" onkeyup="showResult(this.value)" type="search" placeholder="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <div id="livesearch"> hello</div>
         </form>
     </div>
 </nav>
@@ -85,7 +87,7 @@ $conn->close();
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-8">
-                <h1><?php echo $name?> <?php echo $_GET['substance']?></h1>
+                <h1><?php echo $name?></h1>
                 <hr class="my-4"/>
                 <p><?php echo $desc;?></p>
                 <h2>Chemistry</h2>
